063fb7a1cebd8e3997d409e6ebd4d13182126897
Aakash2305e <152877265+Aakash2305e@users.noreply.github.com>
2024-01-02 21:33:33 +0500

    Add files via upload

---
+++ App.js
@@ -0,0 +1,10 @@
+document.write("Name:Farrukh")
+document.write("Name:Farrukh")
+document.write("Name:Farrukh")
+document.write("Name:Farrukh")
+var x = 18;
+var y = 'farrukh'
+var z = [1,2,3];
+document.write(typeof x)
+document.write(typeof y)
+document.write(c[0])
